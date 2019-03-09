10c50b64f033c442924c2c5fbf7a262f2b184a16
scyairax <alessio.ciraci@gmail.com>
2019-03-10 00:04:03 +0100

    slide centrate

--- index.php
+++ index.php
@@ -7,11 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="includes/bootstrap/dist/css/bootstrap.min.css">
+    <!--- mio css--->
+    <link rel="stylesheet" href="css/stile.css">
 
     <title>Hello, world!</title>
   </head>
   <body>
-  <nav class="navbar navbar-expand-lg navbar-light"  style="background-color: #FFA100;">
+  <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -20,78 +22,116 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">Chi siamo<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">I Nostri Partner</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contattaci</a>
       </li>
 
+      
+
     </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-    </form>
+
   </div>
 </nav>
 <!---                                 --->
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-            <div class="bd-example">
-  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="https://via.placeholder.com/600x400/000000/FFFFFF/?text=IPaddress.net" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/600x400/000000/FFFFFF/?text=IPaddress.net" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </div>
-      </div>
-      <div class="carousel-item">
-        <img src="https://via.placeholder.com/600x400/000000/FFFFFF/?text=IPaddress.net" class="d-block w-100" alt="...">
-        <div class="carousel-caption d-none d-md-block">
-          <h5>Third slide label</h5>
-          <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </div>
-      </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+            <div class="col-md-12" id="first_carosel">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" id="item-c" src="http://www.italiafruit.net/Public/Upload/News/Images/46587/frutta-verdura-4.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>sa sa sa</h5>
+    <p>prova</p>
   </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" id="item-c" src="http://www.meteoweb.eu/wp-content/uploads/2017/02/pesce-640x447.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>sa sa sa</h5>
+    <p>prova</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" id="item-c" src="http://2.citynews-trevisotoday.stgy.ovh/~media/original-hi/38318609774681/pizza-3-2.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>sa sa sa</h5>
+    <p>prova</p>
+  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
             </div>
         </div>
-    </div>
+        <div class="row">
+             <div class="col-md-12" id="first_carosel">
 
+             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100 img-responsive" id="item-c" src="https://www.foodweb.it/wp-content/uploads/2018/10/salumi-ivsi-manifesto-made-in-italy-valori.jpg" alt="First slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>sa sa sa</h5>
+    <p>prova</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 img-responsive" id="item-c" src="https://www.delbenechiropratica.it/wp-content/uploads/2016/06/77f8f52f-30de-468d-81ac-64dbb57d911f-1400x933.jpg" alt="Second slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>sa sa sa</h5>
+    <p>prova</p>
+  </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://www.cucinarecarne.it/wp-content/uploads/2013/04/bistecca-alla-fiorentina-575x262.jpg" alt="Third slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>sa sa sa</h5>
+    <p>prova</p>
+  </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+            </div>
+        </div>   
+    </div>
+        <div class="row" id="footer">
+            <div class="col-md-12"> ciao io sono il tuo footer
+            <form class="form-inline my-2 my-lg-0">
+             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+            </form>
+            </div>
+        </div>
 
    
     <!-- Optional JavaScript -->
