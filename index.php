d856375a52b6880f7bbdd87ede2a9358bd72344b
scyairax <alessio.ciraci@gmail.com>
2019-03-10 12:39:43 +0100

    primo template di prova terminato

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    
+    <!-- fontawsome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="includes/bootstrap/dist/css/bootstrap.min.css">
     <!--- mio css--->
@@ -13,55 +17,89 @@
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#"><i class="fas fa-user-tie fa-3x"></i><p id="name_agency">Liddi&Co.</p></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
+
       <li class="nav-item active">
-        <a class="nav-link" href="#">Chi siamo<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="#">| <i class="fas fa-apple-alt"></i>  Chi siamo |<span class="sr-only">(current)</span></a>
       </li>
+
       <li class="nav-item">
-        <a class="nav-link" href="#">I Nostri Partner</a>
+        <a class="nav-link" href="#">| <i class="fas fa-hamburger"></i> I Nostri Partner |</a>
       </li>
+
       <li class="nav-item">
-        <a class="nav-link" href="#">Contattaci</a>
+        <a class="nav-link" href="#">| <i class="fas fa-utensils"></i> Contattaci |</a>
       </li>
+
     </ul>
-  </div>
+    <img src="https://via.placeholder.com/300X110/FF0000/FFFFFF?Text=Down.com" class="rounded float-right" alt="Responsive image">
 </nav>
-<!---         end nav                        --->
+<!---                                 --->
     <div class="container">
         <div class="row">
-        <div class="carousel slide carosello1" data-ride="carousel">
+            <div class="col-md-12">
+            <div id="carouselExampleIndicators" class="delay1 carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://www.italiafruit.net/Public/Upload/News/Images/46587/frutta-verdura-4.jpg" alt="Responsive image">
+      <div class="carousel-caption d-none d-md-block">
+    
+  </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://www.meteoweb.eu/wp-content/uploads/2017/02/pesce-640x447.jpg" alt="Responsive image">
+      <div class="carousel-caption d-none d-md-block">
+   
+  </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://2.citynews-trevisotoday.stgy.ovh/~media/original-hi/38318609774681/pizza-3-2.jpg" alt="Responsive image">
+      <div class="carousel-caption d-none d-md-block">
+   
+  </div>
     </div>
   </div>
 </div>
-
-<div class="carousel slide carosello2" data-ride="carousel">
+            </div>
+        </div>
+        <div class="row">
+             <div class="col-md-12">
+             <div id="carouselExampleIndicators" class="delay2 carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(35).jpg">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://www.italiafruit.net/Public/Upload/News/Images/46587/frutta-verdura-4.jpg" alt="Responsive image">
+      <div class="carousel-caption d-none d-md-block">
+    
+  </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(33).jpg">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://www.meteoweb.eu/wp-content/uploads/2017/02/pesce-640x447.jpg" alt="Responsive image">
+      <div class="carousel-caption d-none d-md-block">
+   
+  </div>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg">
+      <img class="d-block w-100 img-responsive" id="item-c" src="http://2.citynews-trevisotoday.stgy.ovh/~media/original-hi/38318609774681/pizza-3-2.jpg" alt="Responsive image">
+      <div class="carousel-caption d-none d-md-block">
+  </div>
     </div>
   </div>
 </div>
+            </div>
+        </div>   
+    </div>
+        <div class="row" id="footer">
+            <div class="col-md-12"> ciao io sono il tuo footer
+            <form class="form-inline my-2 my-lg-0">
+             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
+            </form>
+            </div>
         </div>
 
    
@@ -70,17 +108,6 @@
     <script src="includes/jquery/dist/jquery.slim.min.js"></script>
     <script src="includes/popper.js/dist/popper.min.js"></script>
     <script src="includes/bootstrap/dist/js/bootstrap.min.js"></script>
-  <script type="text/javascript">
-$(document).ready(function () {
-    $('.carosello1').carousel({
-        interval: 2000
-    });
-    $(document).ready(function () {
-    $('.carosello2').carousel({
-        interval: 1500
-    });
-
-  
-</script>   
-</body>
+    <script src="js/delay_carosello.js"></script>
+  </body>
 </html>
---
+++ js/delay_carosello.js
@@ -0,0 +1,6 @@
+$('.delay1').carousel({
+    interval: 1800
+    })
+$('.delay2').carousel({
+        interval: 3000
+        })
