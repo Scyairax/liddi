cec2eadb745dcb5cc651848b9d104807b8174147
scyairax <alessio.ciraci@gmail.com>
2019-03-12 09:41:17 +0100

    creato sistemino active per il menu, d rivedere.

---
+++ components/navbar.php
@@ -0,0 +1,22 @@
+<?php
+echo '<nav class="navbar navbar-expand-lg navbar-light" id="navbar">
+        <a class="navbar-brand" href="#" id="name_agency"><i class="fas fa-user-tie fa-2x"></i> Cyairax Technology </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item '.$liActive1.' ">
+                    <a class="nav-link" href="index.php">| <i class="fas fa-apple-alt"></i> Chi siamo |<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item '.$liActive2.'  ">
+                    <a class="nav-link" href="partner.php">| <i class="fas fa-hamburger"></i> I Nostri Partner |</a>
+                </li>
+                <li class="nav-item '.$liActive3.'  ">
+                    <a class="nav-link" href="contatti.php">| <i class="fas fa-utensils"></i> Contattaci |</a>
+                </li>
+            </ul>
+            <img src="https://via.placeholder.com/200X60/FF0000/FFFFFF?Text=Down.com" class="rounded float-right" id="name_agency" alt="Responsive image">
+        </div>
+    </nav>';
+    ?>
--- index.php
+++ index.php
@@ -11,31 +11,16 @@
     <link rel="stylesheet" href="includes/bootstrap/dist/css/bootstrap.min.css">
     <!--- mio css--->
     <link rel="stylesheet" href="css/stile.css">
-    <title>Hello, world!</title>
+    <title>Cyairax Technology</title>
 </head>
 
 <body>
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
-        <a class="navbar-brand" href="#" id="name_agency"><i class="fas fa-user-tie fa-2x"></i> Liddi&Co.</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">| <i class="fas fa-apple-alt"></i> Chi siamo |<span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="partner.php">| <i class="fas fa-hamburger"></i> I Nostri Partner |</a>
-                </li>
-                <li class="nav-item  ">
-                    <a class="nav-link" href="contatti.php">| <i class="fas fa-utensils"></i> Contattaci |</a>
-                </li>
-            </ul>
-            <img src="https://via.placeholder.com/200X60/FF0000/FFFFFF?Text=Down.com" class="rounded float-right" id="name_agency" alt="Responsive image">
-        </div>
-    </nav>
+    <?php
+    $liActive1 = "active";
+    $liActive2 = "";
+    $liActive3 = "";
+    include_once "components/navbar.php"; ?>
     <!---   end navbar   --->
     <!---   container   --->
     <div class="container">
