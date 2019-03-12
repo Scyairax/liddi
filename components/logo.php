c0bfada479b1348d5009bbc7119791f8cff12d76
scyairax <alessio.ciraci@gmail.com>
2019-03-12 09:57:46 +0100

    ds

---
+++ components/logo.php
@@ -0,0 +1,12 @@
+<?php
+echo '<div class="row">
+        <div class="row">
+        <div class="col-md-6"><i class="fas fa-bolt"></i></div>
+        <div class="col-md-6"><i class="fas fa-dragon"></i></div>
+        </div>
+        <div class="row">
+        <div class="col-md-6"><i class="fas fa-exclamation-triangle"></i></div>
+        <div class="col-md-6"><i class="fas fa-cogs"></i></div>
+        </div>   
+    </div>';
+?>
