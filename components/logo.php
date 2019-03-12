e48f5c5a91ccabf96d2f2ef77bd1e2af4b149760
scyairax <alessio.ciraci@gmail.com>
2019-03-12 10:01:26 +0100

    sistemato il logo

--- components/logo.php
+++ components/logo.php
@@ -1,12 +1,13 @@
-<?php
-echo '<div class="row">
+ <?php
+
+    echo '<div class="col">
         <div class="row">
-        <div class="col-md-6"><i class="fas fa-bolt"></i></div>
-        <div class="col-md-6"><i class="fas fa-dragon"></i></div>
+        <div class="col-xs-1"><i class="fas fa-bolt"></i></div>
+        <div class="col-xs-1"><i class="fas fa-dragon"></i></div>
         </div>
         <div class="row">
-        <div class="col-md-6"><i class="fas fa-exclamation-triangle"></i></div>
-        <div class="col-md-6"><i class="fas fa-cogs"></i></div>
+        <div class="col-xs-1"><i class="fas fa-exclamation-triangle"></i></div>
+        <div class="col-xs-1"><i class="fas fa-cogs"></i></div>
         </div>   
     </div>';
-?>
+    ?> 
