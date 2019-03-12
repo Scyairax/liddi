06ca40033ff771e35aafb91672c8b0c3843ea855
scyairax <alessio.ciraci@gmail.com>
2019-03-12 09:47:03 +0100

    incluso il componente footer a tutte le pagine

--- partner.php
+++ partner.php
@@ -152,35 +152,7 @@
 
         <hr>
 
-        <div class="row form-group" id="footer">
-            <div class="col-md-4 col-xs-4">
-                <h3 class="footer">Liddi&Co s.r.l.</h3>
-                <hr>
-                <p class="footer"> Via Farcito 65, Baretto </p>
-                <p class="footer"> Centro dir. Milanofsdiori, Pucci (MO) </p>
-                <p class="footer"> P.IVA: 0273xxxx0363 </p>
-            </div>
-
-            <div class="col-md-4 col-xs-4">
-                <h3 class="footer">Quick Links</h3>
-                <hr>
-                <p class="footer">Chi Siamo</p>
-                <p class="footer">I nostri Partner</p>
-                <p class="footer">Come Trovarci</p>
-            </div>
-
-            <div class="col-md-4 col-xs-4">
-                <h3 class="footer">Sei un Fornitore?</h3>
-                <hr>
-                <p class="footer">Vorresti ricevere maggiori informazioni su di noi?</p>
-                <p class="footer"><a href="#">Contattaci<a></p>
-                <p class="footer">Telefono : 0985214663</p>
-                <p class="footer">Fax : 4521589631</p>
-                <p class="footer">Email: afd@dfsdfsdf.comi</p>
-                <form class="float-right form-inline my-2 my-lg-0">
-                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button>
-                </form>
-            </div>
+        <?php include_once "components/footer.php" ?>
 
         </div>
 
